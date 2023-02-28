08b5bfa5fdc3af11b5ca36d8a2750f9c8d327258
rpg2014 <rpg2014@users.noreply.github.com>
2023-02-27 23:21:18 -0800

    Commiting Spencers work on the EC2 Wrapper

--- server/src/wrappers/MinecraftEC2Wrapper.ts
+++ server/src/wrappers/MinecraftEC2Wrapper.ts
@@ -1,37 +1,306 @@
-import { EC2Client, RunInstancesCommandOutput } from "@aws-sdk/client-ec2";
+import {
+	CreateImageCommand,
+	DeleteSnapshotCommand,
+	DeregisterImageCommand,
+	DescribeImagesCommand,
+	DescribeImagesResult,
+	DescribeInstancesCommand,
+	DescribeSnapshotsCommand,
+	DescribeSnapshotsResult,
+	EC2Client,
+	ImageState,
+	RebootInstancesCommand,
+	RunInstancesCommand,
+	RunInstancesCommandOutput,
+	Snapshot,
+	StartInstancesCommand,
+	StopInstancesCommand,
+	TerminateInstancesCommand,
+} from '@aws-sdk/client-ec2';
+
+import {MinecraftDBWrapper} from './MinecraftDynamoWrapper';
 
 export class MinecraftEC2Wrapper {
-    ec2Client: EC2Client
-    constructor() {
-        this.ec2Client = new EC2Client({region: "us-east-1"})
-    }
 
+	private static readonly AMI_NAME = 'Minecraft_Server';
+	private static readonly SECURITY_GROUP_ID = 'sg-0bcf97234db49f1d4';
+	private static readonly AWS_ACCOUNT_ID = process.env.AWS_ACCOUNT_ID || '';
+	private static readonly INSTANCE_TYPE = process.env.EC2_INSTANCE_TYPE || '';
+	private static readonly USER_DATA = 'KGNyb250YWIgLWwgMj4vZGV2L251bGw7IGVjaG8gIiovNSAqICAgKiAgICogICAqICAgd2dldCA' +
+		'tcSAtTyAtICJodHRwczovL2lyb24tc3BpZGVyLmhlcm9rdWFwcC5jb20iID4vZGV2L251bGwgMj4mMSIpIHwgY3JvbnRhYiAtCnNoIG1pbm' +
+		'VjcmFmdC9ydW5fc2VydmVyLnNo';
+	private static readonly SERVER_DETAILS = new MinecraftDBWrapper();
+	private static readonly EC2_CLIENT = new EC2Client({region: 'us-east-1'});
 
-    /**
-     * Public methods
-     */
-    public startInstance(){}
-    public stopInstance(){}
+	private static IMAGE_ID: string | undefined;
+	private static ourInstance: MinecraftEC2Wrapper;
 
-    
+	public static getInstance(): MinecraftEC2Wrapper {
+		if (!MinecraftEC2Wrapper.ourInstance) {
+			MinecraftEC2Wrapper.ourInstance = new MinecraftEC2Wrapper();
+		}
+		return MinecraftEC2Wrapper.ourInstance;
+	}
 
-    public getInstanceDomainName(){}
-    public getInstanceIP(){}
-    public isInstanceUp(){}
-    public isInstanceStopped(){}
-    public getInstanceStatus(){}
+	public async startInstance(): Promise<boolean> {
+		if (!await MinecraftEC2Wrapper.SERVER_DETAILS.isServerRunning() || !await this.isInstanceUp()) {
+			const runInstancesResponse = await MinecraftEC2Wrapper.EC2_CLIENT.send(new RunInstancesCommand({
+				ImageId: await MinecraftEC2Wrapper.SERVER_DETAILS.getAmiId(),
+				InstanceType: MinecraftEC2Wrapper.INSTANCE_TYPE,
+				MaxCount: 1,
+				MinCount: 1,
+				UserData: MinecraftEC2Wrapper.USER_DATA,
+				SecurityGroupIds: [MinecraftEC2Wrapper.SECURITY_GROUP_ID],
+				KeyName: 'Minecraft Server',
+			}));
+			const instanceId = await this.getInstanceId(runInstancesResponse);
+			await MinecraftEC2Wrapper.SERVER_DETAILS.setInstanceId(instanceId);
+			const request = new StartInstancesCommand({InstanceIds: [instanceId]});
+			let success = false;
+			await MinecraftEC2Wrapper.EC2_CLIENT
+				.send(request)
+				.then(response => {
+					success = !!(response.StartingInstances?.[0].CurrentState?.Code &&
+						response.StartingInstances?.[0].CurrentState?.Code < 32);
+				})
+				.catch(error => console.error(error));
+			if (success) {
+				await console.info('Started server');
+				MinecraftEC2Wrapper.IMAGE_ID = await MinecraftEC2Wrapper.SERVER_DETAILS.getAmiId();
+				await MinecraftEC2Wrapper.SERVER_DETAILS.setServerRunning();
+			}
+			return success;
+		} else {
+			return false;
+		}
+	}
 
-    // Optional
-    public rebootInstance(){}
-    /**
-     * Private methods
-     */
-    private getInstanceId(runInstancesResponse: RunInstancesCommandOutput){}
-    private  waitForAMIToBeCreated() {}
-    private waitForSnapshotToBeCreated() {}
-    private waitForServerStop() {}
-    private getNewestSnapshot(){}
-    private deleteOldAMI(oldAMIId: string, oldSnapshotId: string){}
-    private makeAMI(instanceId: string){}
+	private async getInstanceId(runInstancesResponse: RunInstancesCommandOutput): Promise<string> {
+		const instanceList = runInstancesResponse.Instances || [];
+		const idList: string[] = [];
+		for (const instance of instanceList) {
+			if (instance.InstanceId) {
+				idList.push(instance.InstanceId);
+			}
+		}
 
+		if (idList.length === 1) {
+			return idList[0];
+		} else {
+			return 'error';
+		}
+	}
+
+	public async stopInstance(): Promise<boolean> {
+		if (await MinecraftEC2Wrapper.SERVER_DETAILS.isServerRunning() || await this.isInstanceUp()) {
+			const instanceId = await MinecraftEC2Wrapper.SERVER_DETAILS.getInstanceId();
+			const currentAMIId = await MinecraftEC2Wrapper.SERVER_DETAILS.getAmiId();
+			const currentSnapshot = await MinecraftEC2Wrapper.SERVER_DETAILS.getSnapshotId();
+
+			if (MinecraftEC2Wrapper.IMAGE_ID !== currentAMIId) {
+				await console.error(`OLD AMI IS OUT OF DATE, oldami: ${MinecraftEC2Wrapper.IMAGE_ID} , 
+                    dynamoAMI: ${currentAMIId}`);
+			}
+
+			await MinecraftEC2Wrapper.EC2_CLIENT.send(new StopInstancesCommand({
+				InstanceIds: [instanceId]
+			}));
+
+			await this.waitForServerStop(instanceId);
+			await MinecraftEC2Wrapper.SERVER_DETAILS.setServerStopped();
+			const amiId = await this.makeAMI(instanceId);
+			await this.waitForSnapshotToBeCreated();
+			await this.waitForAMIToBeCreated();
+
+			await MinecraftEC2Wrapper.SERVER_DETAILS.setAmiId(amiId);
+			await MinecraftEC2Wrapper.SERVER_DETAILS.setSnapshotId(await this.getNewestSnapshot());
+
+			await console.info('Server Stopped');
+
+			if (currentAMIId && currentSnapshot &&
+				(await MinecraftEC2Wrapper.SERVER_DETAILS.getAmiId() !== currentAMIId)
+				&& (await MinecraftEC2Wrapper.SERVER_DETAILS.getSnapshotId() !== currentSnapshot)) {
+				await console.info('Deleting old snapshot_id');
+				await this.deleteOldAmi(currentAMIId, currentSnapshot);
+			}
+
+			const terminateInstancesResult = await MinecraftEC2Wrapper.EC2_CLIENT.send(new TerminateInstancesCommand({
+				InstanceIds: [instanceId]
+			}));
+
+			const success = !!(terminateInstancesResult.TerminatingInstances?.[0].CurrentState?.Code &&
+				terminateInstancesResult.TerminatingInstances[0].CurrentState.Code > 32);
+			if (success) {
+				await console.info('Terminated Server');
+			}
+
+			await MinecraftEC2Wrapper.SERVER_DETAILS.setServerStopped();
+			return success;
+		} else {
+			return false;
+		}
+	}
+
+	private async waitForAMIToBeCreated(): Promise<void> {
+		let response: DescribeImagesResult;
+
+		do {
+			await console.info('Waiting for ami to be created');
+			await this.sleep(1000);
+
+			response = await MinecraftEC2Wrapper.EC2_CLIENT.send(new DescribeImagesCommand({
+				ExecutableUsers: ['self'], Filters: [{
+					Name: 'state', Values: ['pending', 'failed', 'error']
+				}]
+			}));
+			await console.info(response.toString());
+
+			if (response.Images?.find(image => image.State === ImageState.failed || image.State === ImageState.error)) {
+				await console.error('There is a failed ami');
+			}
+		} while (response.Images?.find(image => image.State === ImageState.pending));
+	}
+
+	private async waitForSnapshotToBeCreated(): Promise<void> {
+		let response: DescribeSnapshotsResult;
+		let finishedSnapshots: Snapshot[] = [];
+
+		do {
+			await console.info('Waiting for snapshot to be created');
+			await this.sleep(5000);
+
+			response = await MinecraftEC2Wrapper.EC2_CLIENT.send(new DescribeSnapshotsCommand({
+				OwnerIds: [MinecraftEC2Wrapper.AWS_ACCOUNT_ID.replace(/-/g, '')]
+			}));
+
+			if (response.Snapshots?.length && response.Snapshots?.length > 1) {
+				finishedSnapshots = response.Snapshots.filter(snapshot => snapshot.Progress?.includes('100'));
+			}
+		} while ((finishedSnapshots.length !== response.Snapshots?.length) && (response.Snapshots?.length === 1));
+	}
+
+	private async waitForServerStop(instanceId: string): Promise<void> {
+		await console.info(`Waiting for instance ${instanceId} to stop`);
+		do {
+			try {
+				await this.sleep(5000);
+			} catch (e) {
+				await console.error(e);
+			}
+		} while (!await this.isInstanceStopped());
+	}
+
+	private async getNewestSnapshot(): Promise<string> {
+		const response = await MinecraftEC2Wrapper.EC2_CLIENT.send(new DescribeSnapshotsCommand({
+			OwnerIds: [MinecraftEC2Wrapper.AWS_ACCOUNT_ID.replace(/-/g, '')]
+		}));
+		const latestDate = new Date(Number.MIN_SAFE_INTEGER).toISOString();
+		let newestSnap: Snapshot = {};
+		response.Snapshots?.forEach(snapshot => {
+			if (snapshot.StartTime && latestDate < snapshot.StartTime.toISOString()) {
+				newestSnap = snapshot;
+			}
+		})
+		await console.info('Newest Snapshot is ' + newestSnap.SnapshotId);
+		return newestSnap.SnapshotId!;
+	}
+
+	private async deleteOldAmi(oldAMIid: string, oldSnapshotId: string): Promise<void> {
+		await MinecraftEC2Wrapper.EC2_CLIENT.send(new DeregisterImageCommand({
+			ImageId: oldAMIid
+		}));
+		await MinecraftEC2Wrapper.EC2_CLIENT.send(new DeleteSnapshotCommand({
+			SnapshotId: oldSnapshotId
+		}));
+	}
+
+	private async makeAMI(instanceId: string): Promise<string> {
+		const createImageResponse = await MinecraftEC2Wrapper.EC2_CLIENT.send(new CreateImageCommand({
+			InstanceId: instanceId, Name: MinecraftEC2Wrapper.AMI_NAME + '-' + Date.now().toString(),
+		}));
+		const amiId = createImageResponse.ImageId!;
+		await console.info('Created AMI, image id: ' + amiId);
+		return amiId;
+	}
+
+	public async rebootInstance(): Promise<void> {
+		if (await MinecraftEC2Wrapper.SERVER_DETAILS.isServerRunning()) {
+			const result = await MinecraftEC2Wrapper.EC2_CLIENT.send(new RebootInstancesCommand({
+				InstanceIds: [await MinecraftEC2Wrapper.SERVER_DETAILS.getInstanceId()]
+			}));
+			await console.info(result.toString(), this.constructor.name);
+		} else {
+			await console.info('Server isn\'t up to be rebooted');
+		}
+	}
+
+	public async getInstanceDomainName(): Promise<string> {
+		const instanceId = await MinecraftEC2Wrapper.SERVER_DETAILS.getInstanceId();
+		const response = await MinecraftEC2Wrapper.EC2_CLIENT.send(new DescribeInstancesCommand({
+			InstanceIds: [instanceId]
+		}));
+		return response.Reservations?.[0].Instances?.[0].PublicDnsName || 'Server is not running';
+	}
+
+	public async getInstanceIp(): Promise<string> {
+		const instanceId = await MinecraftEC2Wrapper.SERVER_DETAILS.getInstanceId();
+		const response = await MinecraftEC2Wrapper.EC2_CLIENT.send(new DescribeInstancesCommand({
+			InstanceIds: [instanceId]
+		}));
+		return response.Reservations?.[0].Instances?.[0].PublicIpAddress || 'Error: Cannot get Instance IP';
+	}
+
+	public async isInstanceUp(): Promise<boolean> {
+		const instanceId = await MinecraftEC2Wrapper.SERVER_DETAILS.getInstanceId();
+		const response = await MinecraftEC2Wrapper.EC2_CLIENT.send(new DescribeInstancesCommand({
+			InstanceIds: [instanceId]
+		}));
+		if (response.Reservations?.length == 0 || response.Reservations?.[0].Instances?.length == 0) {
+			return false;
+		}
+		const isUp = response.Reservations?.[0].Instances?.[0].State?.Code == 16;
+		if (isUp) {
+			await console.info(`Server instance ${instanceId} is up`);
+		} else {
+			await console.info(`Server instance ${instanceId} is down`);
+		}
+		return isUp;
+	}
+
+	public async isInstanceStopped(): Promise<boolean> {
+		const response = await MinecraftEC2Wrapper.EC2_CLIENT.send(new DescribeInstancesCommand({
+			InstanceIds: [await MinecraftEC2Wrapper.SERVER_DETAILS.getInstanceId()]
+		}));
+		if (response.Reservations?.length === 0 || response.Reservations?.[0].Instances?.length === 0) {
+			return true;
+		}
+		const isDown = response.Reservations?.[0].Instances?.[0].State?.Code === 80;
+		if (isDown) {
+			await console.info(
+				`Server Instance ${await MinecraftEC2Wrapper.SERVER_DETAILS.getInstanceId()} is down`);
+		}
+		return isDown;
+	}
+
+	public async getInstanceStatus(): Promise<number> {
+		try {
+			const response = await MinecraftEC2Wrapper.EC2_CLIENT.send(new DescribeInstancesCommand({
+				InstanceIds: [await MinecraftEC2Wrapper.SERVER_DETAILS.getInstanceId()]
+			}));
+			if (response.Reservations && response.Reservations.length === 0) {
+				return 48; // terminated
+			}
+			return response.Reservations?.[0]?.Instances?.[0]?.State?.Code || 0; // 0 = pending
+		} catch (e: any) {
+			if (e.includes('Invalid id') || e.includes('does not exist')) {
+				return 40;
+			}
+			console.error('Cannot determine instance status: ', e);
+			return 0;
+		}
+	}
+
+	private async sleep(ms: number): Promise<void> {
+		return new Promise(resolve => setTimeout(resolve, ms));
+	}
 }
--- server/src/wrappers/Route53Wrapper.ts
+++ server/src/wrappers/Route53Wrapper.ts
@@ -1,10 +1,11 @@
-import { Route53Client } from "@aws-sdk/client-route-53"
+import { ChangeAction, ChangeResourceRecordSetsCommand, ChangeResourceRecordSetsCommandInput, Route53Client, RRType } from "@aws-sdk/client-route-53"
 
 
 
 
 export class Route53Wrapper {
     route53Client: Route53Client
+    private HOSTED_ZONE_ID = "ZSXXJQ44AUHG2";
     constructor() {
         this.route53Client = new Route53Client({region: "us-east-1"})
     };
@@ -12,6 +13,15 @@ export class Route53Wrapper {
     public updateMinecraftDNS(ipAddress: string){
         return this.updateDNSforURL("mc.parkergiven.com", ipAddress)
     }
-    public updateDNSforURL(url: string, ipAddress: string){}
+    public updateDNSforURL(url: string, ipAddress: string){
+        console.log(`Updating url: ${url}, to ip: ${ipAddress}`)
+        const input: ChangeResourceRecordSetsCommandInput = {HostedZoneId: this.HOSTED_ZONE_ID, ChangeBatch: {Changes: [{Action: ChangeAction.CREATE, ResourceRecordSet: {Name: url, ResourceRecords: [{Value: ipAddress}], TTL: 300, Type: RRType.A}}]}}
+        const request: ChangeResourceRecordSetsCommand = new ChangeResourceRecordSetsCommand(input) ;
+        try {
+            const response = await this.route53Client.send(request)
+        }catch (e) {
+            
+        }
+        response.
 
 }
